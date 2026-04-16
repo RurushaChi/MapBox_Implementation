11a71fffab7eab9e28c4ee61b110830af8caf134
Jesse <rchikerema@gmail.com>
2026-04-16 12:59:59 +1200

    custom form and base form connected

--- Enrolment_Form/urls.py
+++ Enrolment_Form/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import enrolment_form_view, create_custom_form_view, custom_form_success_view
 
 urlpatterns = [
-    path("enrol/", enrolment_form_view, name="enrolment_form"),
+    path("enrol/<int:school_id>/", enrolment_form_view, name="enrolment_form"),
     path("schools/<int:school_id>/custom-form/create/", create_custom_form_view, name="create_custom_form"),
     path("custom-form/success/", custom_form_success_view, name="custom_form_success"),
 
